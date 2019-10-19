fc8619f1629cf3e089b0f1a4e2af170ea75d399c
Kaisei Sunaga <jkdwhssk@yahoo.co.jp>
2019-10-19 21:10:59 +0900

    Initial Commit

---
+++ SKExtensions/SKExtensions.h
@@ -0,0 +1,19 @@
+//
+//  SKExtensions.h
+//  SKExtensions
+//
+//  Created by 砂賀開晴 on 2019/10/19.
+//  Copyright © 2019 Kaisei Sunaga. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SKExtensions.
+FOUNDATION_EXPORT double SKExtensionsVersionNumber;
+
+//! Project version string for SKExtensions.
+FOUNDATION_EXPORT const unsigned char SKExtensionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SKExtensions/PublicHeader.h>
+
+
