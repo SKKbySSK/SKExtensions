c58b78e2b5ff74a07e655b11d01538861d1b3c12
Kaisei Sunaga <jkdwhssk@yahoo.co.jp>
2019-10-19 21:24:38 +0900

    Add SIMD4 extension

---
+++ SKExtensions/Extensions/SIMD4.swift
@@ -0,0 +1,21 @@
+//
+//  SIMD4.swift
+//  SKExtensions
+//
+//  Created by 砂賀開晴 on 2019/10/19.
+//  Copyright © 2019 Kaisei Sunaga. All rights reserved.
+//
+
+import Foundation
+import SceneKit
+
+extension SIMD4 where Scalar == Float {
+  var positionMatrix: simd_float4x4 {
+    return simd_float4x4([
+      SIMD4(1, 0, 0, x),
+      SIMD4(0, 1, 0, y),
+      SIMD4(0, 0, 1, z),
+      SIMD4(x, y, z, 1),
+    ])
+  }
+}
